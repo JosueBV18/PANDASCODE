34689f861fb6dd84f0f670d010dffa52948999c1
JOSUEBV18 <MJbv.2300447212@gmail.com>
2025-05-27 09:26:01 -0500

    ClasePandasSem05

--- appGitHub02.py
+++ appGitHub02.py
@@ -23,16 +23,32 @@ print('')
 print('PANDAS DATAFRAME')
 
 df = {'one':pd.Series(data=[100,200,300], index=['Pera','Manzana','Limon']),
-      'two':pd.Series(data=[400,500,600], index=['Pera','Tomate','Lima'])}
+      'two':pd.Series(data=[400,500,600], index=['Pera','Tomate','Limon'])}
 
 df01 = pd.DataFrame(df)
 
-print(df01)
+# print(df01)
 
-print('Index {}'.format(df01.index))
-print('Columns', df01.columns)
+# print('Index {}'.format(df01.index))
+# print('Columns', df01.columns)
 
 df01['three']=df01['one']*df01['two']
 print(df01)
 
+df01['Flag']=df01['three']>200
+print(df01)
+
+aux01 = df01.pop('three')
+print(df01)
+
+df01.insert(2, 'Copy of two', df01['two'])
+df01['FlagTwo']=df01['Copy of two']>200
+aux02 = df01.pop('Copy of two')
+print(df01)
+
+df01['twoUpperHalf'] = df01['two'][:2]
+print(df01)
+
+df01['twoDonwHalf'] = df01['two'][2:]
+print(df01)
 print('_________________________________________')
